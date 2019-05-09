b845a933a8fc992d02b6090aa3dc4c63259e91c7
Ad Friedman <adam.friedman@flatironschool.com>
2019-05-09 16:28:08 -0400

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,55 @@
+class GenresController < ApplicationController
+
+
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+    # @genres = @genre.genres
+  end
+
+  def new
+    @genre = Genre.new()
+  end
+
+
+  def create
+    @genre = Genre.create(genre_params)
+      if @genre.valid?
+        redirect_to genres_path
+      else
+        render "new"
+      end
+  end
+
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    @genre.save
+    redirect_to @genre
+  end
+
+
+  def destroy
+    Genre.find(params[:id]).destroy
+    redirect_to genres_path
+  end
+
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+
+
+
+end
